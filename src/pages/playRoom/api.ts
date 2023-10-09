be789fc36acce94414c1815ac797202ea3c94aa6
bbb169 <827088092@qq.com>
2023-10-09 17:57:42 +0800

    refactor: delete unuse codes

--- src/pages/playRoom/api.ts
+++ src/pages/playRoom/api.ts
@@ -1,60 +1,4 @@
-import { privateKey } from "@/const.js";
 import { emitSocket } from "@/utils/api.js";
-import { AES } from "crypto-js";
-import { useParams } from "react-router-dom";
-import { CardColor, CardType, PlayerInfoType } from "./type.js"
-
-const holderCards: CardType[] = ['hearts', 'spades'].map((item) => {
-  const number = Math.round(Math.random() * 10);
-  return {
-      key: AES.encrypt(item + number, privateKey).toString(),
-      color: item as CardColor,
-      number,
-      showFace: 'front',
-      holder: 'bbb169',
-      statu: 'distributed'
-  }
-})
-
-let currentPlayer = 0;
-
-// function getAllPlayersInfo(): Promise<PlayerInfoType[]> {
-//   currentPlayer = (++currentPlayer)%8 || 8
-
-//   return new Promise((resolve) => {
-//     resolve([3,2,1,8,7,6,5,4].map((item) => {
-//       return {
-//         name: `player-${item}`,
-//         position: item,
-//         status: item === currentPlayer ? 'calling' : 'waiting',
-//         holdCards: item === 4 ? holderCards : undefined,
-//         holdCent: 10,
-//       }
-//     }))
-//   })
-// }
-
-// ==================== split players ==============
-
-let myPlayer: PlayerInfoType;
-
-// export async function getOtherPlayersAndMyPlayer(): Promise<{
-//   myPlayer: PlayerInfoType,
-//   otherPlayers: PlayerInfoType[],
-// }> {
-//   // const players = await getAllPlayersInfo();
-//   // const myPlayer = players.pop() as PlayerInfoType;
-//   // const otherPlayers: PlayerInfoType[] = players;
-
-//   // return {
-//   //   myPlayer,
-//   //   otherPlayers,
-//   // };
-// }
-
-// export async function getMyPlaer() {
-//   return myPlayer || (await getOtherPlayersAndMyPlayer()).myPlayer
-// }
 
 // ========================= call chips =========================
 
@@ -69,35 +13,5 @@ export function playerFold(roomId: string, userName: string) {
   return new Promise((resolve) => {
     emitSocket(`fold:${roomId}:${userName}`)
     resolve('success')
-  })
-}
-
-// ======================== get public cards ====================
-
-const publicCards: CardType[] = ['hearts', 'spades', 'clubs', 'diamonds'].map((item, index) => {
-  const number = Math.round(Math.random() * 10) + index;
-  return {
-      key: AES.encrypt(item + number, privateKey).toString(),
-      color: item as CardColor,
-      number,
-      showFace: 'back',
-      statu: 'distributed',
-  }
-})
-
-export function getPublicCards() {
-  let nextCardIndex = publicCards.findIndex(item => item.showFace === 'back');
-
-  const nextCard = publicCards[nextCardIndex]
-
-  if (nextCard) {
-    publicCards[nextCardIndex] = {
-      ...nextCard,
-      showFace: 'front',
-    }
-  }
-
-  return new Promise<CardType[]>((resolve) => {
-    resolve(publicCards);
   })
 }
